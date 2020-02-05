89cda0126a536899421d564a4e1be4e8cfb83374
Ryan Davis <ryanscdavis@gmail.com>
2020-02-04 20:45:18 -0500

    Clean up

--- src/Graph.js
+++ src/Graph.js
@@ -41,25 +41,17 @@ const create = function (ref, nodes, links) {
     nodes = nodes.map(n => Object.assign(n, { radius: radScale(Math.sqrt(n.size)) }))
 
     svg.selectAll('circle').data(nodes).enter().append('circle')
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
         .attr('r', d => d.radius)
         .attr('fill', 'none')
         .attr('stroke', lineColor)
         .attr('stroke-width', d => d.radius*lineScale)
 
     svg.selectAll('line').data(links).enter().append('line')
-        .attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y)
         .style('stroke', lineColor)
         .attr('fill', 'none')
         .attr('stroke-width', d => 0.5*(d.source.radius + d.target.radius)*lineScale)
 
     svg.selectAll('text').data(nodes).enter().append('text')
-        .attr('x', d => d.x)
-        .attr('y', d => d.y)
         .attr('text-anchor', 'middle')
         .attr('alignment-baseline', 'middle')
         .text(d => d.labelInside)
@@ -87,6 +79,7 @@ const onTick = function (svg, links) {
     svg.selectAll('text')
         .attr('x', d => d.x)
         .attr('y', d => d.y)
+
 }
 
 export default function (props) {
